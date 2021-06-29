6e7323e34a662563679fe8aa90c9e19489ff0181
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-28 20:39:52 -0500

    rack responses lab DONE

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+# (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+
+class Application
+    def call(env)
+        resp = Rack::Response.new
+
+        if Time.now.hour < 12
+            resp.write "Good Morning"
+        else
+            resp.write "Good Afternoon"
+        end
+
+        resp.finish
+
+    end
+end
